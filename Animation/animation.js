b8b793afe3f18cea4e5c7d4ce274ebf169becd34
PUNK's Shihne <shihne@mail.ru>
2018-09-22 16:47:11 +0300

    Три шара, рандомное начальное положение, фукция в draw

--- Animation/animation.js
+++ Animation/animation.js
@@ -15,20 +15,27 @@ function init() {
     var canvas = document.getElementById("game");
     var ctx = canvas.getContext('2d');
     var last_redraw_time = get_time();
-    var x1 = 320;
-    var y1 = 240;
-    var x2 = 345;
-    var y2 = 324;
-    var v = 80;
-    //var SPEED_x = 50;
-    //var SPEED_y = 40;
-    var alpha1 = Math.random() * 2 * Math.PI;
-    var alpha2 = Math.random() * 2 * Math.PI;// не более 180
+    var v = 100;
+    //шар 1
+    var x1 = 90 + Math.random() * 440;
+    var y1 = 90 + Math.random() * 300;
+    var alpha1 = Math.random() * 2 * Math.PI; // не более 180
     var dx1 = Math.cos(alpha1) * v;
     var dy1 = Math.cos(alpha1) * v;
+    //шар2
+    var x2 = 90 + Math.random() * 440;
+    var y2 = 90 + Math.random() * 300;
+    var alpha2 = Math.random() * 2 * Math.PI;
     var dx2 = Math.cos(alpha2) * v;
     var dy2 = Math.cos(alpha2) * v;
-    requestAnimationFrame(animation_step());
+    //шар3
+    var x3 = 90 + Math.random() * 440;
+    var y3 = 90 + Math.random() * 300;
+    var alpha3 = Math.random() * 2 * Math.PI;
+    var dx3 = Math.cos(alpha3) * v;
+    var dy3 = Math.cos(alpha3) * v;
+
+    animation_step();
 
     function draw() {
         //здесь перерисовывается содержимое экрана
@@ -40,13 +47,14 @@ function init() {
         ctx.strokeStyle = "black";
         ctx.strokeRect(50.5, 50.5, 540, 380);
 
-        ctx.beginPath();
-        ctx.arc(x1, y1, 40, 0, Math.PI * 2);
-        ctx.fillStyle = "rgba(230, 1, 77, 0.5)";
-        ctx.fill();
+        create_ball(x1, y1);
+        create_ball(x2, y2);
+        create_ball(x3, y3);
+    }
 
+    function create_ball(x, y) {
         ctx.beginPath();
-        ctx.arc(x2, y2, 40, 0, Math.PI * 2);
+        ctx.arc(x, y, 40, 0, Math.PI * 2);
         ctx.fillStyle = "rgba(230, 1, 77, 0.5)";
         ctx.fill();
     }
@@ -60,24 +68,27 @@ function init() {
 	    допустим, скорость изменения x - это SPEED_x.
 	    для определенности SPEED_x = 10 пикселей в секунду */
         //if (x >= 550 || x <= 90 || y >= 390 || y <= 90)
-        if (x1 >= 550 || x1 <= 90) {
+
+        if (x1 >= 550 || x1 <= 90)
             dx1 = dx1 * (-1);
-            // SPEED_y = SPEED_y * Math.cos(alpha);
-        }
-        if (x2 >= 550 || x2 <= 90) {
-            dx2 = dx2 * (-1)
-        }
-        if (y1 >= 390 || y1 <= 90) {
+        if (y1 >= 390 || y1 <= 90)
             dy1 = dy1 * (-1);
-            // SPEED_x = SPEED_x * Math.cos(alpha);
-        }
-        if (y2 >= 390 || y2 <= 90) {
-            dy2 = dy2 * (-1)
-        }
         x1 += dx1 * elapsed_time_sec;
         y1 += dy1 * elapsed_time_sec;
+
+        if (x2 >= 550 || x2 <= 90)
+            dx2 = dx2 * (-1);
+        if (y2 >= 390 || y2 <= 90)
+            dy2 = dy2 * (-1);
         x2 += dx2 * elapsed_time_sec;
         y2 += dy2 * elapsed_time_sec;
+
+        if (x3 >= 550 || x3 <= 90)
+            dx3 = dx3 * (-1);
+        if (y3 >= 390 || y3 <= 90)
+            dy3 = dy3 * (-1);
+        x3 += dx3 * elapsed_time_sec;
+        y3 += dy3 * elapsed_time_sec;
     }
 
     function animation_step() {

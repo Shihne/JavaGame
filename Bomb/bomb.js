1c67f77e33912c9acc3f236cdbde0125fa87ebd8
PUNK's Shihne <shihne@mail.ru>
2018-10-21 22:35:30 +0300

    Больше переменных

--- Bomb/bomb.js
+++ Bomb/bomb.js
@@ -12,8 +12,8 @@ function init() {
     var canvasBeginX = 105;
     var canvasBeginY = 38;
     var beginXY = 128.5;
-    var endX = 1280 - beginXY * 2;
-    var endY = 720 - beginXY * 2;
+    var rect_width = canvas.width - beginXY * 2;
+    var rect_height = canvas.height - beginXY * 2;
 
     var mouse = {
         x: 0,
@@ -24,12 +24,21 @@ function init() {
         return {
             frame_index: 0,
             num_frames: 648,
+            num_columns: 36,
             str_index: 0,
             x: x,
             y: y,
             dx: dx,
             dy: dy,
-            exploded: false
+            size: 50,
+            frame_size: 100,
+            exploded: false,
+            explosionX_size: 64,
+            explosionY_size: 249 / 4,
+            explosionX_frame_size: 256,
+            explosionY_frame_size: 249,
+            explosion_num_frames: 15,
+            explosion_num_columns: 5
         };
     }
 
@@ -45,23 +54,27 @@ function init() {
         mouse.x = event.clientX - rect.left; //event.pageX - canvasBeginX;
         mouse.y = event.clientY - rect.top; // event.pageY - canvasBeginY;
 
-        for (var i = 0; i < balls.length; i++)
-            if (mouse.x >= balls[i].x && mouse.x <= balls[i].x + 50 && mouse.y >= balls[i].y && mouse.y <= balls[i].y + 50
-            && balls[i].exploded === false) {
-                balls[i].exploded = true;
-                balls[i].num_frames = 15;
-                balls[i].start_time = get_time() / 1000;
-                console.log(true);
-                return;
+        for (var i = 0; i < balls.length; i++) {
+            var ball = balls[i];
+            if (mouse.x >= ball.x && mouse.x <= ball.x + ball.size &&
+                mouse.y >= ball.y && mouse.y <= ball.y + ball.size &&
+                ball.exploded === false) {
+                    ball.exploded = true;
+                    ball.num_frames = 15;
+                    ball.start_time = get_time() / 1000;
+                    console.log(true);
+                    return;
             }
+        }
 
-        if (mouse.x <= beginXY || mouse.y <= beginXY || mouse.x >= 1280 - beginXY || mouse.y >= 720 - beginXY)
+        if (mouse.x <= beginXY || mouse.y <= beginXY ||
+            mouse.x >= canvas.width - beginXY || mouse.y >= canvas.height - beginXY)
             console.log("BAKA!!");
-        else if (mouse.x <= beginXY + (endX - beginXY) / 2 && mouse.y <= beginXY + (endY - beginXY) / 2)
+        else if (mouse.x <= beginXY + (rect_width - beginXY) / 2 && mouse.y <= beginXY + (rect_height - beginXY) / 2)
             balls.push(new_ball(mouse.x, mouse.y, 100, 100));
-        else if (mouse.x <= beginXY + (endX - beginXY) / 2 && mouse.y > beginXY + (endY - beginXY) / 2)
+        else if (mouse.x <= beginXY + (rect_width - beginXY) / 2 && mouse.y > beginXY + (rect_height - beginXY) / 2)
             balls.push(new_ball(mouse.x, mouse.y - 50, 100, -100));
-        else if (mouse.y <= beginXY + (endY - beginXY) / 2)
+        else if (mouse.y <= beginXY + (rect_height - beginXY) / 2)
             balls.push(new_ball(mouse.x - 50, mouse.y, -100, 100));
         else
             balls.push(new_ball(mouse.x - 50, mouse.y - 50, -100, -100));
@@ -72,16 +85,21 @@ function init() {
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.strokeStyle = "black";
-        ctx.strokeRect(beginXY, beginXY, endX, endY);
+        ctx.strokeRect(beginXY, beginXY, rect_width, rect_height);
         ctx.drawImage(aimg, 0, 0);
 
         for (var i = 0; i < balls.length; i++) {
-            if (balls[i].exploded === false) {
-                balls[i].str_index = Math.floor(balls[i].frame_index / 36);
-                balls[i].frame_index = balls[i].frame_index - 36 * balls[i].str_index;
-                ctx.drawImage(bomb, 100 * balls[i].frame_index, 100 * balls[i].str_index, 100, 100, balls[i].x, balls[i].y, 50, 50);
+            var ball = balls[i];
+            if (ball.exploded === false) {
+                ball.str_index = Math.floor(ball.frame_index / ball.num_columns);
+                ball.frame_index = ball.frame_index - ball.num_columns * ball.str_index;
+                ctx.drawImage(bomb, ball.frame_size * ball.frame_index, ball.frame_size * ball.str_index,
+                    ball.frame_size, ball.frame_size, ball.x, ball.y, ball.size, ball.size);
             } else {
-                ctx.drawImage(dimg, 1280 / 5 * balls[i].frame_index, 747 / 3 * balls[i].str_index, 1280 / 5, 748 / 3, balls[i].x, balls[i].y - 25, 256 / 4, 249 / 4);
+                ctx.drawImage(dimg,
+                    ball.explosionX_frame_size * ball.frame_index, ball.explosionY_frame_size * ball.str_index,
+                    ball.explosionX_frame_size, ball.explosionY_frame_size, ball.x, ball.y - ball.size / 2,
+                    ball.explosionX_size, ball.explosionY_size);
             }
         }
     }
@@ -93,20 +111,20 @@ function init() {
             if (ball.exploded === false) {
                 ball.frame_index = Math.floor((current_time_sec - animation_start_time) * FPS) % ball.num_frames;
 
-                if (ball.x >= 1105.5 || ball.x <= 123.5)
+                if (ball.x >= canvas.width - beginXY - ball.size * 0.9 || ball.x <= beginXY - ball.size * 0.1)
                     ball.dx = ball.dx * (-1);
-                if (ball.y >= 545.5 || ball.y <= 123.5)
+                if (ball.y >= canvas.height - beginXY - ball.size * 0.9 || ball.y <= beginXY - ball.size * 0.1)
                     ball.dy = ball.dy * (-1);
                 ball.x += ball.dx * elapsed_time_sec;
                 ball.y += ball.dy * elapsed_time_sec;
             } else {
                 ball.frame_index = Math.floor((current_time_sec - ball.start_time) * FPS);
 
-                if (ball.frame_index > 15)
+                if (ball.frame_index > ball.explosion_num_frames)
                     balls.splice(i, 1);
                 else {
-                    ball.str_index = Math.floor(ball.frame_index / 5);
-                    ball.frame_index = ball.frame_index - ball.str_index * 5;
+                    ball.str_index = Math.floor(ball.frame_index / ball.explosion_num_columns);
+                    ball.frame_index = ball.frame_index - ball.str_index * ball.explosion_num_columns;
                 }
             }
         }
